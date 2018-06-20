27084997149769a62e5f753267804de3b122e108
Leonardo Lima <leonardo.lima.silva@ccc.ufcg.edu.br>
2018-06-20 11:54:24 -0300

    refatorando e deixando mais bonito

--- main.js
+++ main.js
@@ -5,10 +5,9 @@ function Matrix(options) {
 		data = options.data,
 		container = options.container,
 		labelsData = options.labelsMeses,
+		labelsDias = options.labelsDias,
 		startColor = options.start_color,
 		endColor = options.end_color;
-
-	var widthLegend = 100;
 
 	if (!data) {
 		throw new Error('Please pass data');
@@ -34,48 +33,43 @@ function Matrix(options) {
 		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 	var background = svg.append("rect")
-		.style("stroke", "black")
+		.style("stroke", "#ebedf0")
+		.style("fill", "white")
 		.style("stroke-width", "2px")
 		.attr("width", width)
 		.attr("height", height);
 
 	var x = d3.scale.ordinal()
 		.domain(d3.range(numcols))
-		.rangeBands([0, width]);
+		.rangeBands([30, width - 70]);
 
 	var y = d3.scale.ordinal()
 		.domain(d3.range(numrows))
-		.rangeBands([0, height]);
+		.rangeBands([20, height - 60]);
 
 	var colorMap = d3.scale.linear()
-		.domain([minValue, maxValue])
-		.range([startColor, endColor]);
+		.domain([1, 100])
+		.interpolate(d3.interpolateHcl)
+		.range([d3.rgb("#ebe	df0"), d3.rgb('196127')]);
 
 	var row = svg.selectAll(".row")
 		.data(data)
 		.enter().append("g")
 		.attr("class", "row")
-		.attr("transform", function (d, i) { return "translate(0," + y(i) + ")"; });
+		.attr("transform", function (d, i) { return "translate(20," + y(i) + ")"; });
 
 	var cell = row.selectAll(".cell")
 		.data(function (d) { return d; })
 		.enter().append("g")
 		.attr("class", "cell")
-		.attr("transform", function (d, i) { return "translate(" + x(i) + ", 0)"; });
+		.attr("transform", function (d, i) { return "translate(" + x(i) + ", 20)"; });
+
 	cell.append('rect')
 		.attr("width", x.rangeBand())
 		.attr("height", y.rangeBand())
-		.style("stroke-width", 5)
-		.style('stroke', '#ededed');
-
-	cell.append("text")
-		.attr("dy", ".32em")
-		.attr("x", x.rangeBand() / 2)
-		.attr("y", y.rangeBand() / 2)
-		.attr("text-anchor", "middle")
-		.style("fill", function (d, i) { return d >= maxValue / 2 ? 'white' : 'black'; })
-		.text(function (d, i) { return d; });
-
+		.style("stroke-width", 2)
+		.style('stroke', '#ebedf0')
+		.style('stroke', 'white');
 	row.selectAll(".cell")
 		.data(function (d, i) { return data[i]; })
 		.style("fill", colorMap);
@@ -84,92 +78,31 @@ function Matrix(options) {
 		.attr('class', "labels");
 
 	var columnLabels = labels.selectAll(".column-label")
-	    .data(labelsData)
-	    .enter().append("g")
-	    .attr("class", "column-label")
-	    .attr("transform", function(d, i) { return "translate(" + x(i) + "," + height + ")"; });
-
-	columnLabels.append("line")
-		.style("stroke", "black")
-	    .style("stroke-width", "1px")
-	    .attr("x1", x.rangeBand() / 2)
-	    .attr("x2", x.rangeBand() / 2)
-	    .attr("y1", 0)
-	    .attr("y2", 5);
+		.data(labelsData)
+		.enter().append("g")
+		.attr("class", "column-label")
+		.attr("transform", function (d, i) { return "translate(" + x(i * 4) + "," + height + ")"; });
 
 	columnLabels.append("text")
-	    .attr("x", 0)
-	    .attr("y", y.rangeBand() / 2)
-	    .attr("dy", ".82em")
-	    .attr("text-anchor", "end")
-	    .attr("transform", "rotate(-60)")
-	    .text(function(d, i) { return d; });
+		.attr("x", 40)
+		.attr("y", y.rangeBand())
+		.attr("dy", ".82em")
+		.attr("text-anchor", "end")
+		.attr("transform", "rotate(0)")
+		.attr("transform", "translate(5,-200)")
+		.text(function (d, i) { return d; });
 
 	var rowLabels = labels.selectAll(".row-label")
-	    .data(labelsData)
-	  .enter().append("g")
-	    .attr("class", "row-label")
-	    .attr("transform", function(d, i) { return "translate(" + 0 + "," + y(i) + ")"; });
-
-	rowLabels.append("line")
-		.style("stroke", "black")
-	    .style("stroke-width", "1px")
-	    .attr("x1", 0)
-	    .attr("x2", -5)
-	    .attr("y1", y.rangeBand() / 2)
-	    .attr("y2", y.rangeBand() / 2);
+		.data(labelsDias)
+		.enter().append("g")
+		.attr("class", "row-label")
+		.attr("transform", function (d, i) { return "translate(" + 0 + "," + y(i * 2) + ")"; });
 
 	rowLabels.append("text")
-	    .attr("x", -8)
-	    .attr("y", y.rangeBand() / 2)
-	    .attr("dy", ".32em")
-	    .attr("text-anchor", "end")
-	    .text(function(d, i) { return d; });
-
-	var key = d3.select("#legend")
-		.append("svg")
-		.attr("width", widthLegend)
-		.attr("height", height + margin.top + margin.bottom);
-
-	var legend = key
-		.append("defs")
-		.append("svg:linearGradient")
-		.attr("id", "gradient")
-		.attr("x1", "100%")
-		.attr("y1", "0%")
-		.attr("x2", "100%")
-		.attr("y2", "100%")
-		.attr("spreadMethod", "pad");
-
-	legend
-		.append("stop")
-		.attr("offset", "0%")
-		.attr("stop-color", endColor)
-		.attr("stop-opacity", 1);
-
-	legend
-		.append("stop")
-		.attr("offset", "100%")
-		.attr("stop-color", startColor)
-		.attr("stop-opacity", 1);
-
-	key.append("rect")
-		.attr("width", widthLegend / 2 - 10)
-		.attr("height", height)
-		.style("fill", "url(#gradient)")
-		.attr("transform", "translate(0," + margin.top + ")");
-
-	var y = d3.scale.linear()
-		.range([height, 0])
-		.domain([minValue, maxValue]);
-
-	var yAxis = d3.svg.axis()
-		.scale(y)
-		.orient("right");
-
-	key.append("g")
-		.attr("class", "y axis")
-		.attr("transform", "translate(41," + margin.top + ")")
-		.call(yAxis)
+		.attr("x", 40)
+		.attr("y", y.rangeBand() * 2.8)
+		.attr("dy", ".32em")
+		.attr("text-anchor", "end")
+		.text(function (d, i) { return d; });
 
 }
